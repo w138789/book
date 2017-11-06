724747c1a73158d75f33aef4536ab5afce560ccb
w138789 <294496623@qq.com>
2017-11-06 08:44:26 +0800

    11

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -5,27 +5,34 @@ namespace app\index\controller;
 class Index extends Base {
 
     public function index() {
-        $data = model('Book')->order('id DESC')->paginate(10,true);
-        $this->assign('data',$data);
+        $data = model('Book')->order('id DESC')->paginate(10, true);
+        $this->assign('data', $data);
         return $this->fetch();
     }
 
     public function chapter() {
-        $book_id = input('book_id');
-        $data = model('Chapter')->where(['book_id'=>$book_id])->order('id DESC')->paginate(10,true);
-        $book_name = model('Book')->where(['id'=>$book_id])->value('name');
-        $this->assign('data',$data);
-        $this->assign('book_name',$book_name);
+        $book_id   = input('book_id');
+        $data      = model('Chapter')->where(['book_id' => $book_id])->order('id DESC')->paginate(10, true);
+        $book_name = model('Book')->where(['id' => $book_id])->value('name');
+        $this->assign('data', $data);
+        $this->assign('book_name', $book_name);
         return $this->fetch();
     }
 
-    public function info($id = 0){
-        $info = model('Chapter')->where(['id'=>$id])->find();
-        if(empty($info)){
+    public function info($id = 0) {
+        $info = model('Chapter')->where(['id' => $id])->find();
+        if (empty($info)) {
             $this->redirect('/index/index/index');
         }
-        model('Chapter')->where(['id'=>$id])->update(['status'=>1]);
-        $this->assign('info',$info);
+        model('Chapter')->where(['id' => $id])->update(['status' => 1]);
+        $ids     = $id + 1;
+        $map     = [
+            'id'      => $ids,
+            'book_id' => $info['book_id'],
+        ];
+        $next_id = model('Chapter')->where($map)->value('id');
+        $this->assign('info', $info);
+        $this->assign('next_id', $next_id);
         return $this->fetch();
     }
 
