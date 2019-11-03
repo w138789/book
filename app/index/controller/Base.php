275f5329aa3f4127b7140ea3adb8a0453630c4b7
w138789 <294496623@qq.com>
2019-11-03 12:59:58 +0800

    11

--- app/index/controller/Base.php
+++ app/index/controller/Base.php
@@ -155,14 +155,15 @@ class Base extends Controller
                     //获得到此文档中每一个Table对象；
                     $title = $htmDoc->getElementById('nr_title');
                     $text  = $htmDoc->getElementById('nr1');
+                    $htmDoc->encoding = 'UTF-8'; // insert proper
                     //print_r($text->textContent);
                     //exit;
                     //print_r($text);exit;
 
                     //print_r($d[0]);exit;
                     if ($title->textContent && $text->textContent) {
-                        echo $datas['title'] = (iconv("GB2312", "UTF-8", $title->textContent));
-                        $datas['value'] = str_replace('你是天才，一秒记住：三千五中文网，网址:m.cn3k5.com', '', (iconv("GB2312", "UTF-8", $text->textContent)));
+                        echo $datas['title'] = (iconv("GBK", "UTF-8", $title->textContent));
+                        $datas['value'] = str_replace('你是天才，一秒记住：三千五中文网，网址:m.cn3k5.com', '', (iconv("GBK", "UTF-8", $text->textContent)));
                         db('chapter')->insert($datas);
                     }
                     sleep(20);
