73da2b37e04d14ccb7e01fa39d2c561c9da388a8
w138789 <294496623@qq.com>
2019-11-03 13:23:16 +0800

    11

--- app/index/controller/Base.php
+++ app/index/controller/Base.php
@@ -158,7 +158,7 @@ class Base extends Controller
                     //print_r($text->textContent);
                     //exit;
                     //print_r($text);exit;
-                    $data = mb_convert_encoding($text->textContent, 'gb2312', "UTF-8");
+                    $data = mb_convert_encoding($text->textContent, ENT_QUOTES, "UTF-8");
                     print_r($data);exit;
                     //print_r($d[0]);exit;
                     if ($title->textContent && $text->textContent) {
